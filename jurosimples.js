e0017994eb667e240f41b8ec3bba36b997b36fab
jica22 <joaobrsgp@hotmail.com>
2021-10-18 18:33:03 -0300

    Mas isso daqui é uma pica em

--- jurosimples.js
+++ jurosimples.js
@@ -20,94 +20,66 @@ function setInnerHTML(element, value) {
         element.innerHTML = value;
     }
 }
-const dropdown = document.getElementById("dropdownMenu");
-var meses = false;
-document.getElementById("sim").addEventListener("click",function() {
-    meses = true;
-    console.log(meses);
-    dropdown.innerText = "Sim";
-});
-document.getElementById("nao").addEventListener("click",function() {
-    meses = false;
-    console.log(meses);
-    dropdown.innerText = "Não";
-});
 document.getElementById("jurosButton").addEventListener("click",function() {
     var capital = parseFloat(document.getElementById("capital").value);
     var taxa = parseFloat(document.getElementById("taxa").value);
     var tempo = parseFloat(document.getElementById("tempo").value);
+    var juros = parseFloat(document.getElementById("juros").value);
+    var montante = parseFloat(document.getElementById("montante").value);
 
     var textResult = document.getElementById('textResult');
     var textMethod = document.getElementById('textMethod');
 
-if (meses == true) {
-    if (capital && taxa && tempo) {
-        var porcentagem = capital * (taxa/100);
-        var valores = [];
-        console.log("entrei");
-        for (var i = 0; i < tempo; i++) {
-            if (valores.length <= 0) {
-                valores[i] = porcentagem;
-            } else {
-                valores[i] = valores[i-1] + porcentagem;
-            }
-        console.log(valores[i]);
-        }
-        var result = "<strong>Valores pagos</strong> <br />";
-        var method = `<strong>Valor da taxa</strong> <br />
-        P = C * (I/100) <br />
-        P = ${capital} * (${taxa}/${100}) <br />
-        P = ${capital} * ${taxa/100} <br />
-        P = ${capital * (taxa/100)} <br />
-        <br />`;
-        for (valor in valores) {
-            console.log(valor);
-            if (valor == valores.length-1) {
-                result += ` Mês ${parseInt(valor) + 1} = R$${valores[valor]} <br />
-                <br />
-                Resultado: <strong>R$${capital + valores[valor]}</strong>`
-            }
-            if (valor < valores.length-1) {
-            result += ` Mês ${parseInt(valor) + 1} = R$${valores[valor]} <br />`;
-            }
-            if (valor == valores.length-1) {
-                method += `<strong>Custo mês ${parseInt(valor) + 1}</strong> <br />
-                V = M + P <br />
-                V = ${valores[valor] - porcentagem} + ${porcentagem} <br />
-                V = ${valores[valor]} <br />
-                <strong>Custo final</strong> <br />
-                V = C + M + P <br />
-                V = ${capital} + ${valores[valor] - porcentagem} + ${porcentagem} <br />
-                V = ${capital + valores[valor]}`;
-            }
-            if (valor < valores.length-1) {
-                method += `<strong>Custo mês ${parseInt(valor) + 1}</strong> <br />
-                V = M + P <br />
-                V = ${valores[valor] - porcentagem} + ${porcentagem} <br />
-                V = ${valores[valor]} <br /> 
-            `;
-            }
-        }
-        setInnerHTML(textMethod, method);
-        setInnerHTML(textResult, result);
+    if (juros && taxa && tempo) {
+        var result = juros/((taxa/100) * tempo);
+        setInnerHTML(textResult, "<strong>Capital</strong> <br />R$" + (result));
+        setInnerHTML(textMethod, `<strong>Cálculo Capital</strong> <br />
+        J = C*(I/100)*T <br />
+        ${juros} = C*(${taxa}/100)*${tempo} <br />
+        ${juros} = C*${taxa/100}*${tempo} <br />
+        ${juros} = ${(taxa/100)*tempo}C <br />
+        ${(taxa/100)*tempo}C = ${juros} <br />
+        C = ${juros}/${(taxa/100)*tempo} <br />
+        C = ${juros/((taxa/100)*tempo)}
+        `)
     }
-}; 
-    if (meses == false) {
-        if (capital && taxa && tempo) {
-            var result = capital * (taxa/100) * tempo;
-        setInnerText(textResult, "R$" + (capital + result));
-        setInnerHTML(textMethod, `
+    if (montante && taxa && tempo) {
+        var result =  montante/(((taxa/100)*tempo) + 1)
+        setInnerHTML(textResult, "<strong>Capital</strong> <br />R$" + (result));
+        setInnerHTML(textMethod, `<strong>Cálculo Capital</strong> <br />
+        <br />
+        Transformando montante <br />
+        M = C+J <br />
+        ${montante} = C+J <br />
+        C+J = ${montante} <br />
+        J = ${montante} - J
+        J = C*(I/100)*T <br />
+        <br />
+        Calculando Capital <br />
+
+        ${montante} - C = C*(${taxa}/100)*${tempo} <br />
+        ${montante} - C = C*${taxa/100}*${tempo} <br />
+        ${montante} - C = ${(taxa/100)*tempo}C <br />
+        -${(taxa/100)*tempo}C - C = -${montante} <br />
+        -${((taxa/100)*tempo) + 1}C = -${montante} <br />
+        C = -${montante}/(-${((taxa/100)*tempo) + 1}) <br />
+        C = ${montante/(((taxa/100)*tempo) + 1)}
+        `)
+    }
+    if (capital && taxa && tempo) {
+        var result = capital * (taxa/100) * tempo;
+        setInnerText(textResult, + "<br /> <strong>Montante</strong> <br />R$" + (capital + result));
+        setInnerHTML(textMethod, + `<br />
         <strong>Cálculo Juros</strong> <br />
         J = C*(I/100)*T <br />
         J = ${capital} * (${taxa}/100)*${tempo} <br />
         J = ${capital} * ${taxa/100} * ${tempo} <br />
         J = ${capital * (taxa/100) * tempo}<br />
         <br />
-        <strong>Preço final</strong> <br />
+        <strong>Montante</strong> <br />
         M = C + J <br />
         M = ${capital} + ${result} <br />
         M = ${capital + result}
         `)
-        }
     }
-});
+ });
